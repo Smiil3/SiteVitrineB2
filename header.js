9f84991154950b6aa8171fc632213c396d0824de
Smiil3 <baescue18@gmail.com>
2024-10-01 15:08:28 +0200

    Final

--- header.js
+++ header.js
@@ -2,6 +2,7 @@ let button = document.querySelector(".menu-icon")
 button.addEventListener("click", ()=> {
 if (document.querySelector(".iconburger").src === "http://127.0.0.1:8080/img/fermer.png") {
     document.querySelector('main').classList.remove("hidden_phone")
+    document.querySelector('footer').classList.remove("hidden_phone")
     document.querySelector(".navbar").classList.remove("yellow")
     document.querySelector("body").classList.remove("white")
     document.querySelector(".iconburger").src = "img/menu.png"
@@ -12,5 +13,7 @@ if (document.querySelector(".iconburger").src === "http://127.0.0.1:8080/img/fer
     document.querySelector("body").classList.add("white")
     document.querySelector(".iconburger").src = "img/fermer.png"
     document.querySelector(".nav--phone").classList.remove("hidden_phone")
+    document.querySelector('footer').classList.add("hidden_phone")
+
 }
 })
